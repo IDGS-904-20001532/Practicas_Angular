1eedc66b1c53fa32c6180d3f12c8336b9f0df210
Adrian Hernandez <hernandezsanchezluisadrian1@gmail.com>
2023-06-27 19:29:12 -0600

    LAHS. 27/06/2023

--- src/app/cinepolis/cinepolis.component.ts
+++ src/app/cinepolis/cinepolis.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 interface Targeta{
   valor: number;
@@ -13,11 +14,11 @@ interface Targeta{
 })
 export class CinepolisComponent {
     nombre!: string;
-    compradores!: string;
-    cantidad!:string;
-    resultado!:string;
-    valorTarjeta!:string;
-    precioB:number = 12
+    compradores!: number;
+    cantidadBoletos!:number;
+    resultado!:number;
+    valorTarjeta!:number;
+    precioBoleto:number = 12;
     // max=7*this.compradores;
 
     tarjeta: Targeta[]=[
@@ -32,45 +33,73 @@ export class CinepolisComponent {
     ];
 
     comprar(){
+      const maximoBoletos = 7*this.compradores;
+      if(this.cantidadBoletos>maximoBoletos){
+        Swal.fire(
+          'OPS..',
+          'No se pueden comprar mas de 7 boletos!',
+          'info'
+        )
+        this.resultado=0;
+      }else{
+        if(this.cantidadBoletos==3 || this.cantidadBoletos==4 || this.cantidadBoletos==5){
+          if(this.valorTarjeta==1){      
+            const resultado=((this.cantidadBoletos)*this.precioBoleto)-(((this.cantidadBoletos))*(this.precioBoleto)/(100*0.10))
+            const resultado2=((resultado)/(100*0.10)) 
+            this.resultado=resultado-resultado2;
+            Swal.fire(
+              'Exito',
+              'Los boletos secompraroncon exito !!',
+              'success'
+            )
+          }else{
+            const resultado=((this.cantidadBoletos)*this.precioBoleto)-(((this.cantidadBoletos))*(this.precioBoleto)/(100*0.10))
+            this.resultado=resultado;
+            Swal.fire(
+              'Exito',
+              'Los boletos secompraroncon exito !!',
+              'success'
+            )
+          }
+        }else if(this.cantidadBoletos>5){
+          if(this.valorTarjeta==1){
+            const resultado=(((this.cantidadBoletos)*this.precioBoleto)-(((this.cantidadBoletos))*(this.precioBoleto)*(0.15)))
+            const resultado2=((resultado)*(0.10)) 
+            this.resultado=resultado-resultado2;
+            Swal.fire(
+              'Exito',
+              'Los boletos secompraroncon exito !!',
+              'success'
+            )
+          }else{
+            const resultado=(((this.cantidadBoletos)*this.precioBoleto)-(((this.cantidadBoletos))*(this.precioBoleto)*(0.15)))
+            this.resultado=resultado;
+            Swal.fire(
+              'Exito',
+              'Los boletos secompraroncon exito !!',
+              'success'
+            )
+          }
+        }else{
+          if(this.valorTarjeta==1){
+            const resultado=(((this.cantidadBoletos)*this.precioBoleto))
+            const resultado2=((resultado)/(100*0.10))
+            this.resultado=resultado-resultado2;
+            Swal.fire(
+              'Exito',
+              'Los boletos secompraroncon exito !!',
+              'success'
+            )
+          }else{
+            const resultado=(((this.cantidadBoletos)*this.precioBoleto))
+            this.resultado=resultado;
+            Swal.fire(
+              'Exito',
+              'Los boletos secompraroncon exito !!',
+              'success'
+            )
+          }
+        }
+      }
     }
-
-    // if request.method=="POST":
-    // pagoT= (request.form.get("btnTarjeta"))
-    // numP= (request.form.get("txtNumPer"))
-    // precioB = 12
-    // max=7*int(numP)
-    // if(int(numb)>int(max)):
-    //     resp=("No se pueden comprar mas de 7 boletos")
-    //     return render_template("Practica3Cinepolis-main.html",resp=resp)
-    // else:
-    //     if(int(numb)==3 or int(numb)==4 or int(numb)==5):
-    //         if(pagoT=="1"):
-    //             resultado=(int(numb)*precioB)-(float(int(numb))*(precioB)/(100*0.10))
-    //             resultado2=(float(resultado)/(100*0.10))           
-    //             resp="Se ha aplicado 10%, de descuento, ademas de 10%, de descuesto por pagar con tarjeta Cineco, por lo que el precio a pagar es de: ${}".format(str(float(resultado)-(float(resultado2))))  
-    //             return render_template("Practica3Cinepolis-main.html",resp=resp)
-    //         else:
-    //             resultado=(int(numb)*precioB)-(float(int(numb))*(precioB)/(100*0.10))            
-    //             resp=resp="Se ha aplicado 10%, de descuento, por lo que el precio a pagar es de: ${}".format(str(resultado))
-    //             return render_template("Practica3Cinepolis-main.html",resp=resp)
-    //     elif(int(numb)>5):
-    //         if(pagoT=="1"):
-    //             resultado=((int(numb)*precioB)-(float(int(numb))*(precioB)*(0.15)))
-    //             resultado2=(float(resultado)*(0.10))           
-    //             resp="Se ha aplicado 15%, de descuento, ademas de 10%, de descuesto por pagar con tarjeta Cineco, por lo que el precio a pagar es de: ${}".format(str(float(resultado)-(float(resultado2))))  
-    //             return render_template("Practica3Cinepolis-main.html",resp=resp)
-    //         else:
-    //             resultado=(int(numb)*precioB)-(float(int(numb))*(precioB)*(0.15))            
-    //             resp="Se ha aplicado 15%, de descuento, por lo que el precio a pagar es de: ${}".format(str(resultado))
-    //             return render_template("Practica3Cinepolis-main.html",resp=resp)
-    //     else:
-    //         if(pagoT=="1"):
-    //             resultado=(float(int(numb)*precioB))
-    //             resultado2=(float(resultado)/(100*0.10))
-    //             resp="Se ha aplicado 10%, de descuesto por pagar con tarjeta Cineco, por lo que el precio a pagar es de: ${}".format(str(float(resultado)-(float(resultado2))))  
-    //             return render_template("Practica3Cinepolis-main.html",resp=resp)
-    //         else:
-    //             resultado=(int(numb)*precioB)
-    //             resp="El precio a pagar es de: ${}".format(str(resultado))
-    //             return render_template("Practica3Cinepolis-main.html",resp=resp)
 }
